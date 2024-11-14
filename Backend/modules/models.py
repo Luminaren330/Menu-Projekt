2e1a025f1f3e1538eb21338fc4e135b73f74f4a1
Grzegorz Boron <51033813+g-boron@users.noreply.github.com>
2024-11-14 21:27:31 +0100

    feat(models-crud-endpoints): Added new endpoints
    ADDED:
    - GET and POST endpoints for Tables, Dishes, Orders and Reviews models.
    
    CHANGED:
    - changed some model fields

--- Backend/modules/models.py
+++ Backend/modules/models.py
@@ -6,6 +6,7 @@ from flask_login import UserMixin
 from . import db
 
 
+# TODO: Zostawić UserMixin tylko w Accounts, get_id też tylko w Accounts
 class Accounts(db.Model, UserMixin):
   """ Accounts table configuration. """
   account_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -121,27 +122,22 @@ dishes_ingredients = db.Table(
 class Dishes(db.Model, UserMixin):
   """ Dishes table configuration. """
   dish_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  ingredient_id = db.Column(db.Integer,
-                            db.ForeignKey("ingredients.ingredient_id"),
-                            nullable=False)
   category_id = db.Column(db.Integer, db.ForeignKey("categories.category_id"),
                           nullable=False)
-  name = db.Column(db.String(70), nullable=False)
+  name = db.Column(db.String(250), nullable=False)
   price = db.Column(db.Float, nullable=False)
-  photo_url = db.Column(db.String(15), nullable=False)
-  description = db.Column(db.String(40), nullable=False)
+  photo_url = db.Column(db.String(2500), nullable=False)
+  description = db.Column(db.String(350), nullable=False)
 
   ingredients = db.relationship("Ingredients", secondary=dishes_ingredients,
                                 backref="dishes", lazy="dynamic")
   category = db.relationship("Categories", backref="dishes", lazy=True)
 
   def __init__(
-      self, dish_id: int, ingredient_id: int, category_id: int, name: str,
+      self, category_id: int, name: str,
       price: float, photo_url: str, description: str
   ) -> None:
     """ Class constructor """
-    self.dish_id = dish_id
-    self.ingredient_id = ingredient_id
     self.category_id = category_id
     self.name = name
     self.price = price
@@ -189,14 +185,13 @@ class OrderItems(db.Model, UserMixin):
   dish = db.relationship("Dishes", backref="order_items", lazy=True)
 
   def __init__(
-      self, item_id: int, order_id: int, dish_id: int, qunatity: int,
+      self, order_id: int, dish_id: int, quantity: int,
       price: float
   ) -> None:
     """ Class constructor """
-    self.item_id = item_id
     self.order_id = order_id
     self.dish_id = dish_id
-    self.quantity = qunatity
+    self.quantity = quantity
     self.price = price
 
   def __repr__(self) -> str:
@@ -221,23 +216,25 @@ class Orders(db.Model, UserMixin):
   table_reservation_end_time = db.Column(db.DateTime, nullable=True)
   order_status = db.Column(db.String(50), nullable=False)
   order_date = db.Column(db.DateTime, nullable=False, default=datetime.now)
+
   table = db.relationship("Tables", backref="orders", lazy=True)
   account = db.relationship("Accounts", backref="orders", lazy=True)
 
   def __init__(
-      self, order_id: int, table_id: int, account_id: int, total_price: float,
-      take_away: bool, order_status: str, order_date: datetime,
-      update_date: datetime
+      self, account_id: int, total_price: float, order_status: str,
+      order_date: datetime = None, table_start_time: datetime = None,
+      table_end_time: datetime = None, table_id: int = None,
+      take_away_time: datetime = None
   ) -> None:
     """ Class constructor """
-    self.order_id: order_id
     self.table_id = table_id
     self.account_id = account_id
     self.total_price = total_price
-    self.take_away = take_away
+    self.take_away_time = take_away_time
     self.order_status = order_status
     self.order_date = order_date
-    self.update_date = update_date
+    self.table_reservation_start_time = table_start_time
+    self.table_reservation_end_time = table_end_time
 
   def __repr__(self) -> str:
     """ Returns order total price and order date. """
@@ -262,11 +259,10 @@ class Reviews(db.Model, UserMixin):
   account = db.relationship("Accounts", backref="reviews", lazy=True)
 
   def __init__(
-    self, review_id: int, dish_id: int, account_id: int, stars: float,
+    self, dish_id: int, account_id: int, stars: float,
     comment: str
   ) -> None:
     """ Class constructor """
-    self.review_id = review_id
     self.dish_id = dish_id
     self.account_id = account_id
     self.stars = stars
@@ -285,18 +281,17 @@ class Tables(db.Model, UserMixin):
   """ Tables table configuration. """
   table_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
   capacity = db.Column(db.Integer, nullable=False)
-  is_available = db.Column(db.Boolean, nullable=False, default=True)
   description = db.Column(db.String(150), nullable=True)
 
-  def __init__(self, table_id: int, capacity: int, is_available: bool) -> None:
+  def __init__(
+    self, capacity: int, description: str) -> None:
     """ Class constructor """
-    self.table_id = table_id
     self.capacity = capacity
-    self.is_available = is_available
+    self.description = description
 
   def __repr__(self) -> str:
     """ Returns table capacity and status. """
-    return f"Table('{self.capacity}', '{self.is_available}')"
+    return f"Table('{self.capacity}', '{self.description}')"
 
   def get_id(self) -> int:
     """ Returns table id. """
--- Backend/modules/routes.py
+++ Backend/modules/routes.py
@@ -1,10 +1,16 @@
 """ Routes file. """
+from datetime import datetime, timedelta
+
 from flask import request, jsonify, Blueprint, abort
 from flask.wrappers import Response
 from flask_login import login_user, logout_user, login_required, current_user
+from sqlalchemy import or_, and_
 
 from . import db
-from .models import Accounts, Clients, Employee, Categories, Ingredients
+from .models import (
+  Accounts, Clients, Employee, Categories, Ingredients, Tables, Dishes,
+  Orders, OrderItems, Reviews
+)
 
 
 routes = Blueprint("routes", __name__)
@@ -144,8 +150,266 @@ def manage_ingredients() -> tuple[Response, int]:
     return jsonify({"message": "Successfully added new ingredient!"}), 201
 
 
-@routes.route("/login_check", methods=["GET"])
-@login_required
-def login_check() -> tuple[Response, int]:
-  """ Endpoint used in development for checking if user is logged in. """
-  return jsonify({"message": "User is logged in!"}), 200
+@routes.route("/tables", methods=["GET", "POST"])
+def manage_tables() -> tuple[Response, int]:
+  """
+  Endpoint used to manage tables.
+  To add new table use POST method. Pass the following data to the endpoint
+  body: capacity and description.
+  ONLY admin user can add new tables.
+  To retrieve all tables use GET method. Authorized users
+  can retrieve this data. You have to pass query param start_time
+  (%Y-%m-%d %H:%M:%S).
+  """
+  if request.method == "GET":
+    start_time = request.args.get("start_time", datetime(1970, 1, 1, 0, 0, 0))
+    if start_time != datetime(1970, 1, 1, 0, 0, 0):
+      start_time = datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+    end_time = start_time + timedelta(hours=2)
+    tables = (
+      db.session.query(
+        Tables.table_id, Tables.description, Tables.capacity,
+        db.case(
+          (
+            or_(
+              and_(
+                Orders.table_reservation_start_time <= end_time,
+                Orders.table_reservation_start_time >= start_time
+              ),
+              and_(
+                Orders.table_reservation_end_time <= end_time,
+                Orders.table_reservation_end_time >= start_time
+              ),
+              and_(
+                Orders.table_reservation_start_time <= start_time,
+                Orders.table_reservation_end_time >= end_time
+              )
+            ),
+            False
+          ),
+          else_=True
+        ).label("is_available")
+      )
+    ).outerjoin(Orders, Tables.table_id == Orders.table_id).all()
+    response = {"count": len(tables), "records": []}
+    tables_to_return = [
+      {
+        "table_id": table.table_id,
+        "capacity": table.capacity,
+        "description": table.description,
+        "is_available": table.is_available
+      } for table in tables]
+    response["records"] = tables_to_return
+    return jsonify(response), 200
+  elif request.method == "POST":
+    if not current_user.is_authenticated:
+      abort(401)
+    if current_user.role != "admin":
+      abort(403)
+    data = request.get_json()
+    capacity = data.get("capacity")
+    description = data.get("description")
+    table = Tables(
+      capacity=capacity, description=description)
+    db.session.add(table)
+    db.session.commit()
+    return jsonify({"message": "Successfully added new table!"}), 201
+
+
+@routes.route("/dishes", methods=["GET", "POST"])
+def manage_dishes() -> tuple[Response, int]:
+  """
+  Endpoint used to manage dishes.
+  To add new dish use POST method. Pass the following data to the endpoint
+  category (name), ingredients (list of names), name, price, photo_url and
+  description.
+  Admin and employee user can add new dishes.
+  To retrieve all dishes use GET method. Unauthorized users
+  can retrieve this data.
+  """
+  if request.method == "GET":
+    dishes = Dishes.query.all()
+    response = {"count": len(dishes), "records": []}
+    dishes_to_return = [{
+      "name": dish.name,
+      "category": dish.category.name,
+      "ingredients": [ingredient.name for ingredient in dish.ingredients],
+      "price": dish.price,
+      "photo_url": dish.photo_url,
+      "description": dish.description
+    } for dish in dishes]
+    response["records"] = dishes_to_return
+    return jsonify(response), 200
+  elif request.method == "POST":
+    if not current_user.is_authenticated:
+      abort(401)
+    if current_user.role not in ["admin", "employee"]:
+      abort(403)
+    data = request.get_json()
+    category = data.get("category")
+    ingredients = data.get("ingredients")
+    name = data.get("name")
+    price = data.get("price")
+    photo_url = data.get("photo_url")
+    description = data.get("description")
+
+    category = Categories.query.filter_by(name=category).first()
+    ingredients = Ingredients.query.filter(
+      Ingredients.name.in_(ingredients)).all()
+    dish = Dishes(
+      category_id=category.category_id, name=name, price=price,
+      photo_url=photo_url, description=description)
+    dish.ingredients.extend(ingredients)
+
+    db.session.add(dish)
+    db.session.commit()
+    return jsonify({"message": "Successfully added new dish!"}), 201
+
+
+@routes.route("/orders", methods=["GET", "POST"])
+def manage_orders() -> tuple[Response, int]:
+  """
+  Endpoint used to manage orders.
+  To add new order use POST method. Pass the following data to the endpoint:
+  order to the table - table_id, table_reservation_start_time
+  (%Y-%m-%d %H:%M:%S), dishes ([[dish_id, quantity, price per dish]],
+  take away order - take_away_time (%Y-%m-%d %H:%M:%S), dishes.
+  To retrieve all orders use GET method. You can retrieve only take_away orders
+  or orders to the table passing query param ?take_away=true/false.
+  To retrieve specific user's orders pass query param ?account_id=account_id.
+  By not passing param all orders will be retrieved. Only authorized users can
+  retrieve this data.
+  """
+  if request.method == "GET":
+    try:
+      take_away = request.args.get("take_away", None).lower()
+    except AttributeError:
+      take_away = None
+    try:
+      account_id = request.args.get("account_id", None).lower()
+    except AttributeError:
+      account_id = None
+    conditions = []
+
+    if take_away == "true":
+      conditions.append(Orders.take_away_time.isnot(None))
+    elif take_away == "false":
+      conditions.append(Orders.take_away_time.is_(None))
+    if account_id:
+      conditions.append(Orders.account_id == account_id)
+
+    orders = Orders.query.with_entities(
+      Orders.order_id, Orders.table_id, Orders.total_price,
+      Orders.order_status, Orders.take_away_time,
+      Orders.table_reservation_start_time, Orders.table_reservation_end_time,
+      Orders.order_date
+    ).filter(*conditions).all()
+    response = {"count": len(orders), "records": []}
+    orders_to_return = [
+      {
+        "table_id": order.table_id,
+        "total_price": order.total_price,
+        "order_status": order.order_status,
+        "take_away_time": order.take_away_time.strftime("%Y-%m-%d %H:%M:%S")
+        if order.take_away_time else None,
+        "table_reservation_start_time": order.table_reservation_start_time.
+        strftime("%Y-%m-%d %H:%M:%S") if order.table_reservation_start_time
+        else None,
+        "table_reservation_end_time": order.table_reservation_end_time.
+        strftime("%Y-%m-%d %H:%M:%S") if order.table_reservation_end_time
+        else None,
+        "order_items": [
+          {
+            "dish_name": Dishes.query.filter_by(
+              dish_id=order_item.dish_id).first().name,
+            "quantity": order_item.quantity,
+            "price_per_dish": order_item.price
+          }
+          for order_item in OrderItems.query.filter_by(
+            order_id=order.order_id).all()],
+        "order_date": order.order_date.strftime("%Y-%m-%d %H:%M:%S")
+      } for order in orders]
+    response["records"] = orders_to_return
+    return jsonify(response), 200
+  elif request.method == "POST":
+    if not current_user.is_authenticated:
+      abort(401)
+    data = request.get_json()
+    # orders
+    table_id = data.get("table_id")
+    account_id = current_user.get_id()
+    take_away_time = data.get("take_away_time")
+    table_start_time = data.get("table_reservation_start_time")
+    if take_away_time:
+      take_away_time = datetime.strptime(
+        take_away_time, "%Y-%m-%d %H:%M:%S")
+      table_start_time = None
+      table_end_time = None
+    else:
+      take_away_time = None
+      table_start_time = datetime.strptime(
+        table_start_time, "%Y-%m-%d %H:%M:%S")
+      table_end_time = table_start_time + timedelta(hours=2)
+    order_status = "new"
+
+    # order items
+    dishes = data.get("dishes")  # [[dish_id, quantity, price], ...]
+    total_price = round(sum(dish[2] * dish[1] for dish in dishes), 2)
+    order = Orders(
+      table_id=table_id, account_id=account_id, total_price=total_price,
+      take_away_time=take_away_time,
+      table_start_time=table_start_time,
+      table_end_time=table_end_time, order_status=order_status
+    )
+    db.session.add(order)
+    db.session.flush()
+
+    for dish in dishes:
+      dish_id = dish[0]
+      quantity = dish[1]
+      price = dish[2]
+      order_item = OrderItems(
+        order_id=order.order_id, dish_id=dish_id, quantity=quantity,
+        price=price
+      )
+      db.session.add(order_item)
+
+    db.session.commit()
+
+    return jsonify({"message": "Successfully added new order!"}), 201
+
+
+@routes.route("/reviews", methods=["GET", "POST"])
+def manage_reviews() -> tuple[Response, int]:
+  """
+  Endpoint used to manage reviews.
+  To add new review use POST method. Pass the following data to the endpoint
+  dish_id, stars, comment. Only authorized users can add new review.
+  To retrieve all reviews use GET method. You have to pass query param dish_id.
+  """
+  if request.method == "GET":
+    dish_id = request.args.get("dish_id")
+    reviews = Reviews.query.filter_by(dish_id=dish_id).all()
+    response = {"count": len(reviews), "records": []}
+    reviews_to_return = [{
+      "dish": review.dish.name,
+      "user": review.account.email,
+      "stars": review.stars,
+      "comment": review.comment
+    } for review in reviews]
+    response["records"] = reviews_to_return
+    return jsonify(response), 200
+  elif request.method == "POST":
+    if not current_user.is_authenticated:
+      abort(401)
+    data = request.get_json()
+    dish_id = data.get("dish_id")
+    account_id = current_user.get_id()
+    stars = data.get("stars")
+    comment = data.get("comment")
+
+    review = Reviews(
+      dish_id=dish_id, account_id=account_id, stars=stars, comment=comment)
+    db.session.add(review)
+    db.session.commit()
+    return jsonify({"message": "Successfully added new review!"}), 201
